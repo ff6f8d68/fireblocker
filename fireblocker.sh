dc3df4a8b9a21705bb5dcb2d5f6583b77e2f92af
unknow <138153020+ff6f8d68@users.noreply.github.com>
2024-09-03 02:39:48 -0700

    Create fireblocker.sh

---
+++ fireblocker.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Define menu options
+OPTIONS=("Start Fireblocker" "Stop Fireblocker" "Run Low-Intensity Scan" "Run Deep Scan" "Exit")
+SELECTED=0
+
+# Draw the menu
+draw_menu() {
+    clear
+    echo "===================="
+    echo "  Fireblocker Menu  "
+    echo "===================="
+    for i in "${!OPTIONS[@]}"; do
+        if [ $i -eq $SELECTED ]; then
+            echo -e "\e[7m${OPTIONS[$i]}\e[0m"  # Highlight selected option
+        else
+            echo "  ${OPTIONS[$i]}"
+        fi
+    done
+}
+
+# Handle user input
+while true; do
+    draw_menu
+    read -rsn1 input
+
+    case "$input" in
+        $'\x1b')  # Detect arrow keys
+            read -rsn2 -t 0.1 input
+            case "$input" in
+                '[A')  # Up arrow
+                    ((SELECTED--))
+                    if [ $SELECTED -lt 0 ]; then
+                        SELECTED=$((${#OPTIONS[@]} - 1))
+                    fi
+                    ;;
+                '[B')  # Down arrow
+                    ((SELECTED++))
+                    if [ $SELECTED -ge ${#OPTIONS[@]} ]; then
+                        SELECTED=0
+                    fi
+                    ;;
+            esac
+            ;;
+        '')  # Enter key
+            case ${OPTIONS[$SELECTED]} in
+                "Start Fireblocker")
+                    echo "Starting Fireblocker..."
+                    sudo ./fireblocker.sh start
+                    ;;
+                "Stop Fireblocker")
+                    echo "Stopping Fireblocker..."
+                    sudo ./fireblocker.sh stop
+                    ;;
+                "Run Low-Intensity Scan")
+                    echo "Running Low-Intensity Scan..."
+                    sudo ./fireblocker.sh scan-low
+                    ;;
+                "Run Deep Scan")
+                    echo "Running Deep Scan..."
+                    sudo ./fireblocker.sh scan-deep
+                    ;;
+                "Exit")
+                    exit 0
+                    ;;
+            esac
+            read -p "Press any key to return to the menu..." key
+            ;;
+    esac
+done
